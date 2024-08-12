3ec14fcfd06cac55d176ce92168725605f0daf11
ar-itio <162964720+ar-itio@users.noreply.github.com>
2024-08-12 17:32:48 +0530

    Update QrController.java
    test

--- src/main/java/com/example/demo/QrController.java
+++ src/main/java/com/example/demo/QrController.java
@@ -1,1 +1,44 @@
 
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/QR")
+public class QrController {
+
+    @Autowired
+    private QrService qRService;
+
+    @GetMapping("/encrypt")
+    public String encryptData() {
+        try {
+            return qRService.encryptDataToEncrypt();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error occurred while encrypting data";
+        }
+    }
+
+    @GetMapping("/decrypt")
+    public String decryptData(@RequestParam String encryptedData) {
+        try {
+            return qRService.decryptData(encryptedData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error occurred while decrypting data: " + e.getMessage();
+        }
+    }
+
+    @GetMapping("/sign")
+    public String signData() {
+        try {
+            return qRService.signData();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error occurred while signing data";
+        }
+    }
+}
+
