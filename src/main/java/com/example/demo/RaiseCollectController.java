dca937dc4e0cec137fd75608b23cd1cb43a5b92d
ar-itio <162964720+ar-itio@users.noreply.github.com>
2024-08-12 16:18:32 +0530

    Update RaiseCollectController.java
    test

--- src/main/java/com/example/demo/RaiseCollectController.java
+++ src/main/java/com/example/demo/RaiseCollectController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/encryption")
+@RequestMapping("/api/raiseCollect")
 public class RaiseCollectController {
 
     @Autowired
