1585a05374ab48d305eae97f50eaa212887ac44d
ar-itio <162964720+ar-itio@users.noreply.github.com>
2024-08-23 15:47:23 +0530

    Update VpaEnqueryService.java
    test

--- src/main/java/com/example/demo/VpaEnqueryService.java
+++ src/main/java/com/example/demo/VpaEnqueryService.java
@@ -50,8 +50,8 @@ public class VpaEnqueryService {
 	        + "    \"channel\": \"api\",\n"
 	        + "    \"mid\": \"SKYWALK001\",\n"
 	        + "    \"terminalId\": \"\",\n"
-	        + "    \"sid\": \"LETSPE0016\",\n"
-	        + "    \"batch_id\": \"172440691033821897\",\n"
+	        + "    \"sid\": \"LETSPE0018\",\n"
+	        + "    \"batch_id\": \"172440800547814901\",\n"
 	        + "    \"checksum\": \"adifaopdfiojkenwhdfiasdifsf==\"\n"
 	        + "}";
 
@@ -75,8 +75,8 @@ public class VpaEnqueryService {
 		        + "                \"channel\": \"api\",\n"
 		        + "                \"mid\": \"SKYWALK001\",\n"
 		        + "                \"terminalId\": \"\",\n"
-		        + "                \"sid\": \"LETSPE0016\",\n"
-		        + "                \"batch_id\": \"172440691033821897\",\n"
+		        + "                \"sid\": \"LETSPE0018\",\n"
+		        + "                \"batch_id\": \"172440800547814901\",\n"
 		        + "                \"checksum\": \"adifaopdfiojkenwhdfiasdifsf==\"\n"
 		        + "            }\n"
 		        + "        }\n"
